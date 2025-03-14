6896563e64494973c2767cccf571be4c82d873f8
SC314 <scmgaming144@gmail.com>
2025-03-14 00:34:04 +0000

    Update LSButtonsSignup.jsx

--- frontend/src/LoginScreen/LSComponents/LSButtons/LSButtonsSignup.jsx
+++ frontend/src/LoginScreen/LSComponents/LSButtons/LSButtonsSignup.jsx
@@ -1,3 +1,22 @@
+import React, { useState } from 'react';
+import useUsernameStore from './LSButtonsStore/LSUsername';
+import usePasswordStore from './LSButtonsStore/LSPassword';
+import useLoggedIn from "../../LoginScreenStore";
+
+function LSButtonsSignup() {
+
+    const [signupMessage, setSignupMessage] = useState("signup")
+    const { username } = useUsernameStore();
+    const { password } = usePasswordStore();
+    const { setLoggedIn} = useLoggedIn();
+
+    function timedMessage(msg) {
+        setSignupMessage(msg)
+        setTimeout(() => {
+            setSignupMessage("signup")
+        }, 2000)
+    }
+    
 function handleClick() {
     fetch(`${import.meta.env.VITE_API_URL}/signup`, {
         method: "POST",
@@ -20,3 +39,12 @@ function handleClick() {
         timedMessage("Signup failed: Network error");
     });
 }
+
+
+
+    return(
+        <button id="LSButtonsSignup" onClick={handleClick}>{signupMessage}</button>
+    )
+};
+
+export default LSButtonsSignup;
